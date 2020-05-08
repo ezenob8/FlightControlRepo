7e159f37dd5d42f1bb552d0b852e82b643d3763d
claucapo <claucapo@gmail.com>
2020-05-08 12:43:27 -0300

    map google

--- FlightControl/FlightControlWeb/ClientApp/src/app/app.component.ts
+++ FlightControl/FlightControlWeb/ClientApp/src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef, AfterViewInit } from "@angular/core";
+import { MapsAPILoader, GoogleMapsAPIWrapper, AgmMap } from "@agm/core";
+// import "scriptjs";
+// import { get } from "scriptjs";
+// import { ClusterManager } from "@agm/js-marker-clusterer";
+declare var google;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-map",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  bounds = null;
+
+  constructor(private mapsAPILoader: MapsAPILoader) {
+    this.mapsAPILoader.load().then(() => {
+      this.bounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(51.130739, -0.868052), // SW
+        new google.maps.LatLng(51.891257, 0.559417) // NE
+      );
+      console.log(this.bounds);
+    });
+
+  }
+
+  protected mapReady(map) {
+    map.fitBounds(this.bounds);
+  }
 }
