7bf455237d1b468e67056bed608e91baf2043419
Ezequiel Glocer <ezequiel.glocer@live.biu.ac.il>
2020-05-13 02:24:28 +0300

    FlightContext

---
+++ FlightControl/FlightControlWeb/Model/DataAccess/FlightContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightControlWeb.Model.DataAccess
+{
+    public class FlightContext : DbContext
+    {
+        public FlightContext(DbContextOptions<FlightContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
--- FlightControl/FlightControlWeb/Startup.cs
+++ FlightControl/FlightControlWeb/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using FlightControlWeb.Model.DataAccess;
 
 namespace FlightControlWeb
 {
@@ -24,6 +26,9 @@ namespace FlightControlWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<FlightContext>(opt =>
+               //opt.UseSqlServer(
+               //opt.UseInMemoryDatabase("FlightsDB"));
             services.AddControllersWithViews().AddNewtonsoftJson();
             
             // In production, the Angular files will be served from this directory
